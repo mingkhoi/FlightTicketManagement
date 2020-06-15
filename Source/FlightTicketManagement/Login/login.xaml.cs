07140bb089439322757bcf10412a4fa0c6618f52
mingkhoi <mingkhoitran1234@gmail.com>
2020-06-16 03:10:18 +0700

    change GUI

---
+++ Source/FlightTicketManagement/Login/login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace FlightTicketManagement
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : UserControl
+    {
+        public login() {
+            InitializeComponent();
+        }
+
+        private void onMainAppClose(object sender, EventArgs e) {
+            MainWindow.Instance.Close();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e) {
+            MainWindow.Instance.Hide();
+
+            MainApp newApp = new MainApp();
+            newApp.Closed += new EventHandler(onMainAppClose);
+            newApp.Show();
+        }
+
+        private void signUp_Click(object sender, RoutedEventArgs e) {
+            MainWindow.Instance.switchToSignUp();
+        }
+    }
+}
---
+++ Source/FlightTicketManagement/MainApp/MainApp.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlightTicketManagement
+{
+    /// <summary>
+    /// Interaction logic for MainApp.xaml
+    /// </summary>
+    public partial class MainApp : Window
+    {
+        bool isMaximize = false;
+
+        public MainApp() {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private void maximizeBtn_Click(object sender, RoutedEventArgs e) {
+            this.isMaximize = !this.isMaximize;
+
+            if (this.isMaximize) {
+                this.WindowState = WindowState.Maximized;
+            }
+            else this.WindowState = WindowState.Normal;
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e) {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void menuBtn_Click(object sender, RoutedEventArgs e) {
+        }
+
+        private void tileBar_MouseDown(object sender, MouseButtonEventArgs e) {
+            this.DragMove();
+        }
+
+        private void homeBtn_Click(object sender, RoutedEventArgs e) {
+            this.turnOnHomeScreen();
+        }
+
+        private void turnOffHomeScreen() {
+            this.homeGrid.Visibility = Visibility.Hidden;
+        }
+
+        private void turnOnHomeScreen() {
+            this.homeGrid.Visibility = Visibility.Visible;
+            this.scrollView.Visibility = Visibility.Hidden;
+            this.scrollView.Visibility = Visibility.Visible;
+        }
+    }
+}
